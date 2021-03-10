d71a64452f370213fb864943b554c76b2e08b1a1
sivaprasad <sivaprasadmutluru@gmail.com>
2021-03-10 06:17:46 -0800

    firstcommit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () =>{
+    return(
+        <div>
+            <center>
+                &copy;DeveloperConsole
+            </center>
+        </div>
+    )
+}
+export default Footer
---
+++ src/components/signupdata.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+
+
+class signupdata extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            status:'',
+            message:'',
+            role:'',
+            isUserExists:''
+        }
+    }
+     
+    handleChange 
+
+    
+    render(){
+        return(
+           <button onClick={this.handleChange}>status</button>
+        )
+        
+    }
+}
+export default signupdata
