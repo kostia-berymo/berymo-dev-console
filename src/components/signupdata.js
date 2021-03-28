bc7942bed37b0cb63b97d42e8441fcf7df4bad05
sivaprasad <sivaprasadmutluru@gmail.com>
2021-03-27 20:14:14 -0700

    second commit

--- src/components/signupdata.js
+++ src/components/signupdata.js
@@ -1,24 +1,51 @@
 import React,{Component} from 'react';
+import axios from 'axios';
 
 
 class signupdata extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            status:'',
-            message:'',
-            role:'',
-            isUserExists:''
-        }
+       constructor(props){
+           super(props)
+       this.state  =  {
+            value: []
     }
+       }
      
-    handleChange 
-
+    componentDidMount() {
+        axios.get(`assets/samplejson/signup.json`)
+        .then(res=>{
+            console.log(res);
+            this.setState({value: res.data});
+            console.log(this.state.value)
+            
+        });
+    }
+    testfun(){
+        alert(this.state.value.role + '\n'+
+            this.state.value.status +'\n'+
+            this.state.value.message +'\n'+
+            this.state.value.isUserExists)
+    
+    
+    }
     
     render(){
         return(
-           <button onClick={this.handleChange}>status</button>
-        )
+            
+            <center>
+        <div className='panel panel-primary'>
+        <div className="panel-heading">
+         <div className="panel-body">
+          State:  {this.state.value.status}
+         <div>Role: {this.state.value.role}</div>
+         <div>Message:  {this.state.value.message}</div>
+         <div>IsuserExists:   {this.state.value.isUserExists}</div>
+     </div>
+    </div>
+    </div>
+    <button onClick={()=>this.testfun()}>show</button>
+   </center>
+            
+        ) 
         
     }
 }
